fcfaa1e2ebbef09490ac410f3e77da87217792b5
NWCUMallen <78869966+NWCUMallen@users.noreply.github.com>
2021-02-12 13:44:36 -0800

    V2 with Logs

---
+++ Rest-Auth.ps1
@@ -0,0 +1,24 @@
+﻿
+$TenantID = '8b19ff35-468a-47f2-8d1a-d8b0392309cb'
+$ClientID= '25dc0583-b5ca-456e-8288-a8fd6a72bb4b'
+$SecretID = 'uGwH1lH10.jY_Ef-R_4~4.H36~HAbiq2.E'
+
+$RestURI = 'https://login.microsoftonline.com/8b19ff35-468a-47f2-8d1a-d8b0392309cb/oauth2/v2.0/token'
+$body = "client_id=$ClientID&client_secret=$SecretID&scope=https://graph.microsoft.com/.default&grant_type=client_credentials"
+$RestMethod = 'POST' 
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.add('Content-Type','application/x-www-form-urlencoded')
+
+
+
+
+$response = Invoke-RestMethod $RestURI  -Method $RestMethod -Headers $headers -ContentType $RestContentType -Body $body
+$response | ConvertTo-Json
+
+$token = $response.access_token
+$headers.add('Authorization', 'Bearer $token')
+
+
+
+$QuertyResponse = Invoke-RestMethod "https://graph.microsoft.com/v1.0/users/279e1da5-b6df-4f23-9df8-1e6afc644f29"  -Method "GET" -Headers $headers 
+$QueryResponse | ConvertTo-Json
---
+++ Runtime-Script-Trouble.ps1
@@ -0,0 +1,194 @@
+﻿<#
+
+        .DESCRIPTION
+            This Script integrates Appworx with Jira. It Creates and updates tickets based on the runid.
+
+        .NOTES
+            ScriptName   : Jira Integration
+            Created by   : Appworx_Team
+            Date Coded   : 
+
+#>
+##########################################################################################################################
+# Script Requirements ####################################################################################################
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+##########################################################################################################################
+# Application Variables ##################################################################################################
+###### System Variables Needed for Script ################################################################################
+#$Agent = (GI -Path Env:Agent).Value
+#$comp = (GI -Path Env:Computername).Value
+#$jobalias = (GI -Path Env:module).Value
+#$job = (GI -Path Env:job).Value
+#$runid = (GI -Path Env:run_id).Value
+#$AttachmentPath = (GI -Path Env:stdout).Value
+
+### For Testing ###
+#$Agent = 
+#$comp = 
+$jobalias = 'Test'
+$AttachmentPath = 'C:\Temp\Random_Log.txt'
+$job = 'Test'
+$runid = '1122.06'
+
+##########################################################################################################################
+# Script Variables #######################################################################################################
+###### Required Variables ################################################################################################
+[int]$ticketcheck = $runid.Substring($runid.IndexOf('.') + 1, $runid.Length - $runid.IndexOf('.') -1)
+$Aborted = ($ticketcheck + 1)
+[int]$tid = $runid.Substring(0,$runid.IndexOf('.'))
+
+
+##########################################################################################################################
+#JIRA Variables ##########################################################################################################
+###### JSON Values: System ###############################################################################################
+$Token = "Basic SmlyYVNlcnZpY2VBY2NvdW50QG53Y3UuY29tOlQzcGlCRmFvRHBvdzU0MWlTTmpjOTZGOQ=="
+$Jira_Summary= '$runid || $jobalias'
+$Jira_Project = '10446'
+$JiraProjectKey = 'APWX'
+$Jira_IssueType = '10614'
+$Jira_Description = '$jobalias has aborted.'
+
+###### JSON Values: Application Fields ###################################################################################
+$Jira_CF_JobName = $Job
+$Jira_CF_RunID = $tid
+
+##########################################################################################################################
+# Log Variables ##########################################################################################################
+$LogFolder = 'C:\Temp\'
+$LogName = "$TID-$Job.txt"
+
+$LogLocation = $LogFolder + $LogName
+
+##########################################################################################################################
+
+
+If ($ticketcheck -eq 0)
+{
+Start-Transcript -Path $LogLocation
+Write-Host '================================================================================================================'
+Write-host "Ticket not yet Created $ticketcheck"
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $Token)
+$headers.Add("Content-Type", "application/json")
+
+
+  
+ $body = "{
+`n    `"fields`":  {
+`n                   `"summary`":  `"$Jira_Summary`",
+`n                   `"project`":  {
+`n                                   `"id`":  `"$Jira_Project`"
+`n                               },
+`n                   `"issuetype`":  {
+`n                                     `"id`":  `"$Jira_IssueType`"
+`n                                 },
+`n                    `"customfield_11405`" : `"$Jira_CF_JobName`", 
+`n                    `"customfield_11404`" : `"$Jira_CF_RunID`",
+`n                    `"description`": {
+`n      `"type`": `"doc`",
+`n      `"version`": 1,
+`n      `"content`": [
+`n        {
+`n          `"type`": `"paragraph`",
+`n          `"content`": [
+`n                        {
+`n                        `"text`": `"$Jira_Description`",
+`n                        `"type`": `"text`"
+`n                        }
+`n                     ]
+`n                }
+`n            ]
+`n        }
+`n    }
+`n}"
+
+Write-host 'Submitting information to Jira'
+Write-Host '================================================================================================================'
+Write-Host 'Remote Jira Response:'
+Write-host ''
+
+$response = Invoke-RestMethod 'https://nwcu.atlassian.net/rest/api/3/issue/' -Method 'POST' -Headers $headers -Body $body
+$response | ConvertTo-Json
+
+Write-Host '================================================================================================================'
+
+$jirakey = $response.key
+
+$TicketURL = 'https://nwcu.atlassian.net/rest/api/latest/issue/' + $jirakey + '/attachments'
+
+Write-Host "Ticket Creation Successful, Jira Key is $Jirakey"
+
+sleep 2
+
+function Upload-JiraAttachment
+{
+    $wc = new-object System.Net.WebClient
+    $wc.Headers.Add("X-Atlassian-Token", "no-check")
+    $wc.Headers.Add("Authorization", $Token)
+    $wc.UploadFile($TicketURL, $AttachmentPath) > $null
+}
+
+
+$AttachmentResponse = Upload-JiraAttachment
+$AttachmentResponse | ConvertTo-Json
+}
+
+
+Else
+{
+
+Start-Transcript -Path $LogLocation -Append
+Write-Host '================================================================================================================'
+
+$GETURL = 'https://nwcu.atlassian.net/rest/api/3/search?jql=project%20%3D%20'+ $JiraProjectKey +'%20and%20"Run%20ID%5BShort%20text%5D"%20~%20"' + $tid + '"&fields=id'
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $Token)
+$headers.Add("Content-Type", "application/json")
+
+Write-host "Ticket already Created $ticketcheck" 
+Write-Host 'Checking $GETURL to Receive Ticket Key'
+Write-Host '================================================================================================================'
+Write-Host 'Remote Jira Response:'
+Write-host ''
+
+$getResponse = Invoke-RestMethod $GETURL -Method 'GET' -Headers $headers 
+$getResponse | ConvertTo-Json
+
+Write-Host '================================================================================================================'
+
+$UpdateURL = ''
+$UpdateURL = $getresponse.issues.self.ToString()
+$UpdateURL = $UpdateURL+ '/comment'
+
+Write-host ''
+write-host "URL for Updating Ticket is: $UpdateURL"
+ 
+$body = "{
+`n  `"body`": {
+`n    `"type`": `"doc`",
+`n    `"version`": 1,
+`n    `"content`": [
+`n      {
+`n        `"type`": `"paragraph`",
+`n        `"content`": [
+`n          {
+`n            `"text`": `"$Jira_Description`",
+`n            `"type`": `"text`"
+`n          }
+`n        ]
+`n      }
+`n    ]
+`n  }
+`n}"
+ 
+Write-Host '================================================================================================================'
+Write-Host 'Remote Jira Response:'
+$response = Invoke-RestMethod $UpdateURL -Method 'POST' -Headers $headers -Body $body
+$response | ConvertTo-Json
+}
+
+Write-Host '================================================================================================================'
+Stop-Transcript
